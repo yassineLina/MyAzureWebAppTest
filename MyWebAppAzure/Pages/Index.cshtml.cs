81b9e0dc2151939cadf8d8b58fb4b4689013453c
yassineelkharmoudi <yassine.elkharmoudi.cons@autorita-trasporti.it>
2025-10-09 11:00:01 +0200

    aggiunto greeting

--- MyWebAppAzure/Pages/Index.cshtml.cs
+++ MyWebAppAzure/Pages/Index.cshtml.cs
@@ -6,10 +6,12 @@ namespace MyWebAppAzure.Pages;
 public class IndexModel : PageModel
 {
     private readonly ILogger<IndexModel> _logger;
+    private readonly IConfiguration _config;
 
-    public IndexModel(ILogger<IndexModel> logger)
+    public IndexModel(ILogger<IndexModel> logger, IConfiguration config)
     {
         _logger = logger;
+        _config = config;
     }
 
     public void OnGet()
